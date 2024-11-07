4bd6d3cc45524ad0e3c15559499e96d4741e8991
Victor Ribeiro <contato.victoribeiro@gmail.com>
2024-11-07 18:39:10 -0300

    Loggin Middleware

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
 
+app.use(logger)
+
 app.get ('/', (req, res) => {
-    res.send('Home Psge')
+    console.log('Home Page');
+    res.send('Home Page')
 })
 
 app.get('/users', (req, res) => {
+    console.log('Users Page');
     res.send('Users Page')
 })
+
+function logger(req, res, next) {
+    console.log('log');
+    next()
+}
 
 app.listen(3000)
